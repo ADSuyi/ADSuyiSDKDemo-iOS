d0fb902c6aebafedff26f2f2a3351c2d04f5fd07
芭乐 <1372432518@qq.com>
2021-07-19 16:28:51 +0800

    appid修改

--- ADSuyiSDKDemo-iOS/AppDelegate.m
+++ ADSuyiSDKDemo-iOS/AppDelegate.m
@@ -293,7 +293,7 @@ ADSuyiSDKSplashAdDelegate
     // 设置日志输出等级
     [ADSuyiSDK setLogLevel:ADSuyiKitLogLevelDebug];
     // ADSuyiSDK初始化
-    [ADSuyiSDK initWithAppId:@"3766846" completionBlock:^(NSError * _Nonnull error) {
+    [ADSuyiSDK initWithAppId:@"3437764" completionBlock:^(NSError * _Nonnull error) {
         if (error) {
             NSLog(@"SDK 初始化失败：%@", error.localizedDescription);
         }
