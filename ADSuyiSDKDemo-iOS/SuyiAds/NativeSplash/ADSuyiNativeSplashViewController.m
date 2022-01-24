83a7bacde039a500ef2531aa71576a0b118732f8
bale <bale@admobile.top>
2022-01-24 15:22:39 +0800

    自渲染信息流开屏示例

---
+++ ADSuyiSDKDemo-iOS/SuyiAds/NativeSplash/ADSuyiNativeSplashViewController.m
@@ -0,0 +1,32 @@
+//
+//  ADSuyiNativeSplashViewController.m
+//  ADSuyiSDKDemo-iOS
+//
+//  Created by Erik on 2022/1/24.
+//  Copyright © 2022 陈坤. All rights reserved.
+//
+
+#import "ADSuyiNativeSplashViewController.h"
+
+@interface ADSuyiNativeSplashViewController ()
+
+@end
+
+@implementation ADSuyiNativeSplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
